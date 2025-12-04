3f49feb2f3d8ceb8921a56aff2c42cdb823d1770
kyle <lelegrau04@gmail.com>
2025-12-04 23:03:35 +0100

    feat(2024/day_04): implement part1 solution to count occurrences of XMAS and SAMX patterns

--- aoc_2024/day_04/solution.py
+++ aoc_2024/day_04/solution.py
@@ -13,7 +13,33 @@ def parse_input(filename: str) -> str:
 
 def part1(data: str) -> int:
     # TODO: Implement solution
-    return 0
+    search_pattern1 = "XMAS"
+    search_pattern2 = "SAMX"
+    occurences = 0
+    # Checks for left & right overlaps
+    for i in range(len(data) - 3):
+        if data[i:i+4] == search_pattern1 or data[i:i+4] == search_pattern2:
+            occurences += 1
+    # Checks for up & down overlaps
+    data_strings = data.split("\n")
+    for n in range(len(data_strings)):
+        for m in range(len(data_strings[n]) - 3):
+            if data_strings[n][m:m+4] == search_pattern1 or data_strings[n][m:m+4] == search_pattern2:
+                occurences += 1
+    # Checks for diagonal overlaps
+    rows = len(data_strings)
+    cols = len(data_strings[0])
+    for r in range(rows - 3):
+        for c in range(cols - 3):
+            diag1 = data_strings[r][c] + data_strings[r+1][c+1] + data_strings[r+2][c+2] + data_strings[r+3][c+3]
+            diag2 = data_strings[r][c+3] + data_strings[r+1][c+2] + data_strings[r+2][c+1] + data_strings[r+3][c]
+            if diag1 == search_pattern1 or diag1 == search_pattern2:
+                occurences += 1
+            if diag2 == search_pattern1 or diag2 == search_pattern2:
+                occurences += 1
+
+    print("Occurrences of XMAS or SAMX:", occurences)
+    return occurences
 
 
 def part2(data: str) -> int:
