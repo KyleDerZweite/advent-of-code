631c2f809d96a75e3d4d18bd935483d501612a34
kyle <lelegrau04@gmail.com>
2025-12-05 09:55:19 +0100

    feat(2025/day_05): refactor parse_string to improve performance and update part1 logic

--- aoc_2025/day_05/solution.go
+++ aoc_2025/day_05/solution.go
@@ -16,38 +16,72 @@ func parse_input(input string) string {
 	return string(data)
 }
 
-func parse_string(input string) (*Set, *Set) {
-	fresh_ids_set := NewSet()
-	available_ingredient_ids_set := NewSet()
+// func parse_string(input string) (*Set, *Set) {
+// 	// Is shit because takes ages!
+// 	fresh_ids_set := NewSet()
+// 	available_ingredient_ids_set := NewSet()
+//
+// 	fresh_id_lines := strings.Split(strings.TrimSpace(strings.Split(input, "\n\n")[0]), "\n")
+// 	available_id_lines := strings.Split(strings.TrimSpace(strings.Split(input, "\n\n")[1]), "\n")
+// 	for _, line := range fresh_id_lines {
+// 		fresh_ingredient_id_start, _ := strconv.Atoi(strings.Split(line, "-")[0])
+// 		fresh_ingredient_id_end, _ := strconv.Atoi(strings.Split(line, "-")[1])
+// 		for i := fresh_ingredient_id_start; i <= fresh_ingredient_id_end; i++ {
+// 			fresh_ids_set.Add(strconv.Itoa(i))
+// 		}
+// 	}
+//
+// 	for _, line := range available_id_lines {
+// 		available_ingredient_ids_set.Add(strings.TrimSpace(line))
+// 	}
+// 	return fresh_ids_set, available_ingredient_ids_set
+// }
+
+func parse_string(input string) (map[int]int, []int) {
+	// Is shit because takes ages!
+	fresh_id_ranges := map[int]int{}
+	ingredient_ids := []int{}
 
 	fresh_id_lines := strings.Split(strings.TrimSpace(strings.Split(input, "\n\n")[0]), "\n")
-	available_id_lines := strings.Split(strings.TrimSpace(strings.Split(input, "\n\n")[1]), "\n")
+	ingredient_id_lines := strings.Split(strings.TrimSpace(strings.Split(input, "\n\n")[1]), "\n")
+
 	for _, line := range fresh_id_lines {
 		fresh_ingredient_id_start, _ := strconv.Atoi(strings.Split(line, "-")[0])
 		fresh_ingredient_id_end, _ := strconv.Atoi(strings.Split(line, "-")[1])
-		for i := fresh_ingredient_id_start; i <= fresh_ingredient_id_end; i++ {
-			fresh_ids_set.Add(strconv.Itoa(i))
+		fresh_id_ranges[fresh_ingredient_id_start] = fresh_ingredient_id_end
+	}
+
+	for _, line := range ingredient_id_lines {
+		ingredient_id, _ := strconv.Atoi(strings.TrimSpace(line))
+		ingredient_ids = append(ingredient_ids, ingredient_id)
+	}
+	return fresh_id_ranges, ingredient_ids
+}
+
+func check_id_between_range(check_id int, range_start int, range_end int) bool {
+	return check_id >= range_start && check_id <= range_end
+}
+
+func part1(fresh_id_ranges map[int]int, ingredient_ids []int) int {
+	fresh_id_count := 0
+	for _, ingredient_id := range ingredient_ids {
+		for range_start, range_end := range fresh_id_ranges {
+			if check_id_between_range(ingredient_id, range_start, range_end) {
+				fresh_id_count++
+				break
+			}
 		}
 	}
-
-	for _, line := range available_id_lines {
-		available_ingredient_ids_set.Add(strings.TrimSpace(line))
-	}
-	return fresh_ids_set, available_ingredient_ids_set
+	return fresh_id_count
 }
 
-func part1(expected_input_fresh_ids *Set, expected_input_available_ids *Set) int {
-	return expected_input_fresh_ids.Intersection(expected_input_available_ids).Size()
-}
-
-func part2(expected_input_fresh_ids *Set, expected_input_available_ids *Set) int {
+func part2(fresh_id_ranges map[int]int, ingredient_ids []int) int {
 	return 0
 }
 
 func test() {
 	// Test with example data from the puzzle
-	example_input_str := `
-	3-5
+	example_input_str := `3-5
 10-14
 16-20
 12-18
@@ -59,19 +93,19 @@ func test() {
 17
 32`
 
-	expected_input_fresh_ids, expected_input_available_ids := parse_string(example_input_str)
+	fresh_id_ranges, ingredient_ids := parse_string(example_input_str)
 
 	excpeted_part1 := 3
 	excpeted_part2 := 0
 
-	result_part1 := part1(expected_input_fresh_ids, expected_input_available_ids)
+	result_part1 := part1(fresh_id_ranges, ingredient_ids)
 	if result_part1 != excpeted_part1 {
 		fmt.Printf("Part 1 failed: got %d, expected %d, difference %d\n", result_part1, excpeted_part1, excpeted_part1-result_part1)
 	} else {
 		fmt.Println("Part 1 passed")
 	}
 
-	result_part2 := part2(expected_input_fresh_ids, expected_input_available_ids)
+	result_part2 := part2(fresh_id_ranges, ingredient_ids)
 	if result_part2 != excpeted_part2 {
 		fmt.Printf("Part 2 failed: got %d, expected %d, difference %d\n", result_part2, excpeted_part2, excpeted_part2-result_part2)
 	} else {
@@ -84,7 +118,7 @@ func main() {
 	test()
 
 	data := parse_input("input.txt")
-	expected_input_fresh_ids, expected_input_available_ids := parse_string(data)
-	fmt.Println("Part 1 - Max Joltage Sum (2):", part1(expected_input_fresh_ids, expected_input_available_ids))
-	fmt.Println("Part 2 - Max Joltage Sum (12):", part2(expected_input_fresh_ids, expected_input_available_ids))
+	fresh_id_ranges, ingredient_ids := parse_string(data)
+	fmt.Println("Part 1 - Fresh ID Count:", part1(fresh_id_ranges, ingredient_ids))
+	fmt.Println("Part 2 - Max Joltage Sum (12):", part2(fresh_id_ranges, ingredient_ids))
 }
