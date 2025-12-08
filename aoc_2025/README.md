59c7018541de8fb8aed8cb849bc1a7aefc714d60
kyle <lelegrau04@gmail.com>
2025-12-08 09:41:58 +0100

    feat(2025/day_08): implement part 1 logic for Playground puzzle; add input parsing and update test cases

--- aoc_2025/README.md
+++ aoc_2025/README.md
@@ -13,7 +13,7 @@ My solutions for [Advent of Code 2025](https://adventofcode.com/2025).
 | 05 | [Cafeteria](https://adventofcode.com/2025/day/5) ⭐ | ⭐ | [solution.go](day_05/solution.go) |
 | 06 | [Trash Compactor](https://adventofcode.com/2025/day/6) | ⭐ | ⭐ | [solution.go](day_06/solution.go) |
 | 07 | [Laboratories](https://adventofcode.com/2025/day/7) | ⭐ | ⭐ | [solution.go](day_07/solution.go) |
-| 08 | [Day 8](https://adventofcode.com/2025/day/8) | | | |
+| 08 | [Playground](https://adventofcode.com/2025/day/8) | | | |
 | 09 | [Day 9](https://adventofcode.com/2025/day/9) | | | |
 | 10 | [Day 10](https://adventofcode.com/2025/day/10) | | | |
 | 11 | [Day 11](https://adventofcode.com/2025/day/11) | | | |
--- aoc_2025/day_08/solution.go
+++ aoc_2025/day_08/solution.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func parse_input(input string) string {
@@ -14,22 +15,69 @@ func parse_input(input string) string {
 	return string(data)
 }
 
+func is_letter_digit(r rune) bool {
+	return (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') // a-Z and 0-9 matching
+}
+
 func part1(input string) int {
 	// Implement the logic for part 1 of the puzzle here
-	joltage_sum := 0
-	return joltage_sum
+	unique_antinote_locations := 0
+
+	locations_set := make(map[string]bool)
+	lines := strings.Split(input, "\n")
+
+	is_char_set := false
+
+	// Find the current character and its position
+	for y, line := range lines {
+		for x, char := range line {
+			if is_letter_digit(char) && !is_char_set {
+				is_char_set = true
+			} else {
+				continue
+			}
+			for i := 0; i < len(lines); i++ {
+				for j := 0; j < len(lines[i]); j++ {
+					other_char := lines[i][j]
+					if rune(other_char) != char || (y == i && x == j) {
+						continue
+					}
+					x_distance := j - x
+					y_distance := i - y
+					loc_key := fmt.Sprintf("%d,%d", x_distance, y_distance)
+					locations_set[loc_key] = true
+					// fmt.Printf("Distances: '%d':'%d' Chars: %s:%s\n", x_distance, y_distance, string(char), string(other_char))
+				}
+			}
+			is_char_set = false
+		}
+	}
+
+	unique_antinote_locations = len(locations_set)
+	return unique_antinote_locations
 }
 
 func part2(input string) int {
-	joltage_sum := 0
-	return joltage_sum
+	unique_antinote_locations := 0
+	return unique_antinote_locations
 }
 
 func test() {
 	// Test with example data from the puzzle
-	example_input_str := ""
+	example_input_str := `............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............`
 
-	excpeted_part1 := 0
+	excpeted_part1 := 14
 	excpeted_part2 := 0
 
 	result_part1 := part1(example_input_str)
@@ -51,7 +99,7 @@ func main() {
 	// Run tests
 	test()
 
-	// data := parse_input("input.txt")
-	// fmt.Println("Part 1 - Max Joltage Sum (2):", part1(data))
+	data := parse_input("input.txt")
+	fmt.Println("Part 1 - Unique antinode locations:", part1(data))
 	// fmt.Println("Part 2 - Max Joltage Sum (12):", part2(data))
 }
